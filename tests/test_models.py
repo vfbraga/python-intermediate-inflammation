73b1e86d73d57994e4a5b4efb98e7f7cba1a6fef
VittorioINAF <vittorioinaf@msi-vittorioinaf.dyn.iac.es>
2022-05-24 14:49:18 +0100

    parametrization of tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,16 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -30,6 +40,17 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+    ([[1.3, 2.3], [3.3, 4.3], [5.3, 6.3]], [5.3, 6.3]),
+    ([[1.3, 2.3], [3.3, 44.3], [95.3, 6.3]], [95.3, 44.3]),
+    ])
+def test_daily_max_param(test, expected):
+    from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
+
+
 def test_daily_max():
     """Test that max function works for an array of positive integers."""
     from inflammation.models import daily_max
@@ -41,6 +62,16 @@ def test_daily_max():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_max(test_input), test_result)
+
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+    ([[1.3, 2.3], [3.3, 4.3], [5.3, 6.3]],[1.3, 2.3]),
+    ([[1.3, 2.3], [3.3, -44.3], [95.3, -6.3]], [1.3, -44.3]),
+    ])
+def test_daily_min_param(test,expected):
+    from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 def test_daily_min():
     """Test that min function works for an array of positive integers."""
